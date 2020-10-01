028f87e9b7d6aeae78683bcc7d5f3fed5b605224
Carson Uecker-Herman <ueckerhc@mail.gvsu.edu>
2020-09-30 21:06:00 -0400

    Updates

---
+++ precache-manifest.6a9d0bbfa5c69a729f12ebc941ae7e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e1ccd905a41c754be842b8e3dfae0a",
+    "url": "/team-civil/index.html"
+  },
+  {
+    "revision": "886f8ec405dc9a1d8094",
+    "url": "/team-civil/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89a61205c05cb7587375",
+    "url": "/team-civil/static/css/main.dfa3b7cc.chunk.css"
+  },
+  {
+    "revision": "886f8ec405dc9a1d8094",
+    "url": "/team-civil/static/js/2.b7eeb3fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/team-civil/static/js/2.b7eeb3fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a61205c05cb7587375",
+    "url": "/team-civil/static/js/main.6f8965f4.chunk.js"
+  },
+  {
+    "revision": "11fa83eb656d3bd9e54f",
+    "url": "/team-civil/static/js/runtime-main.6a128237.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1407c573042483d8356ab168d9ed9f4a.js"
+  "/team-civil/precache-manifest.6a9d0bbfa5c69a729f12ebc941ae7e81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/team-civil/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
